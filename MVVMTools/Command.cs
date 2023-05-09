d2a7434d443d585ac28f60b6dee03d311b0b3cbd
Semen Teplov <eburg-severca@mail.ru>
2023-05-09 23:03:30 +0500

    Add files via upload

---
+++ MVVMTools/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVMTools
+{
+    public class Command<T> : CommandBase, ICommand
+    {
+        public Command(Action<T> action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        private Action<T> action;
+
+        public void Execute(object? parameter)
+        {
+            action?.Invoke((T)parameter);
+        }
+    }
+
+    public class Command : CommandBase, ICommand
+    {
+        public Command(Action<object> action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        private Action<Object> action;
+
+        public void Execute(object? parameter)
+        {
+            action?.Invoke(parameter);
+        }
+    }
+}
---
+++ MVVMTools/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMTools
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnChanged<T>(out T prop, T value, [CallerMemberName] string proName = "")
+        {
+            prop = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proName));
+        }
+    }
+}
